cbd17e65812bd581445e6d172ad1704e842b02fd
domiebett <dbett49@gmail.com>
2018-08-24 23:40:08 +0300

    chore(tests): adds tests for controllers

--- app/helpers/ride_request_helper.rb
+++
@@ -1,2 +0,0 @@
-module RideRequestHelper
-end
--- test/controllers/devise_controller_test.rb
+++ test/controllers/devise_controller_test.rb
@@ -3,6 +3,15 @@
 require 'test_helper'
 
 class DeviseControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = {
+      first_name: 'Dominic',
+      last_name: 'Bett',
+      email: 'dbett49@gmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+  end
 
   test 'get register user page' do
     get new_user_registration_path
@@ -29,6 +38,25 @@ class DeviseControllerTest < ActionDispatch::IntegrationTest
     }
 
     assert_response :success
+  end
+
+  test 'register existent user fails' do
+    post user_registration_path, params: { user: @user }
+
+    assert_match 'Email has already been taken', response.body
+  end
+
+  test 'login wrong password fails' do
+    post user_session_path,
+         params: { email: 'dbett49@gmail.com', password: 'wrongpassword' }
+
+    assert_match 'Invalid Email or password', response.body
+  end
+
+  test 'non logged in user cannot access app' do
+    get home_index_path
+
+    assert_redirected_to new_user_session_path
   end
 
   test 'logout' do
