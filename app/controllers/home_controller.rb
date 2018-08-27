ada1ec695c57433fb752b2af3a035c3d2d89c40b
domiebett <dbett49@gmail.com>
2018-08-27 16:59:55 +0300

    feat(filter): add location filters
    This commit adds ability for user on the homepage to filter
    the rides displayed by location, i.e by destination and origin.
    It uses the `/filter` route with url parameters.
    
    [Finishes #159696135]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index
-    @rides = Ride.where('departure_date > ?', 1.day.ago)
-                 .order(created_at: :desc).to_a
+  before_action :fetch_rides
+  after_action :remove_full_rides
 
-    @rides.select! do |ride|
+  def index; end
+
+  def filter
+    @rides = @rides.filter_origin(params[:origin]) if params[:origin].present?
+    @rides = @rides.filter_destination(params[:destination]) if params[:destination].present?
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def fetch_rides
+    @rides = Ride.where('departure_date > ?', 1.day.ago)
+                 .order(created_at: :desc)
+  end
+
+  def remove_full_rides
+    @rides.to_a.select! do |ride|
       ride.capacity_left.positive? && (ride.owner != current_user)
     end
   end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ride < ApplicationRecord
   validates :origin, :destination, :departure_date, :departure_time,
             :description, presence: true
@@ -6,6 +8,9 @@ class Ride < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :ride_requests, dependent: :destroy
   has_many :requesters, through: :ride_requests, class_name: 'User', source: :user
+
+  scope :filter_origin, ->(origin) { where('lower(origin) like ?', "%#{origin.downcase}%") }
+  scope :filter_destination, ->(destination) { where('lower(destination) like ?', "%#{destination.downcase}%") }
 
   def capacity_left
     accepted_users_count = ride_requests.where('accepted': true).count
